00660d844f6ae59298de68073ab0a3c439fd66df
schung40 <165240226+schung40@users.noreply.github.com>
2024-04-18 08:49:07 +0000

    x

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/process_roll") do
+  erb(:results)
 end
