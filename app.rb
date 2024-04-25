f39dc560ce910f86da4bf58d83d91968e7003e09
schung40 <165240226+schung40@users.noreply.github.com>
2024-04-25 04:13:08 +0000

    ddd

--- app.rb
+++ app.rb
@@ -6,6 +6,16 @@ get("/") do
   erb(:homepage)
 end
 
+
 get("/process_roll") do
+  @num_dice = params['dice'].to_i
+  @sides = params['sides'].to_i
+
+  @rolls = []
+  @num_dice.times do
+    die = rand(1..@sides)
+    @rolls.push(die)
+  end
+
   erb(:results)
 end
